2e8595c207cd8dfa7f0bbf8ab7a64f5a0f392311
asaraya <asaraya153@hotmail.com>
2021-05-13 08:54:24 -0600

    Colas y funciones básicas

--- Bebes/FactoryS.h
+++ Bebes/FactoryS.h
@@ -2,3 +2,60 @@
 #define FACTORYS_H
 
 #endif // FACTORYS_H
+struct Nodo {
+    int dato;
+    Nodo * siguiente;
+
+    Nodo(int d){
+        dato = d;
+        siguiente = 0;
+    }
+};
+
+struct colaCorazones{
+    Nodo * frente;
+
+    colaCorazones(){
+        frente = 0;
+    }
+    void encolar (int dato);
+    Nodo* desencolar (void);
+    Nodo* verFrente(void);
+    bool vacia(void);
+};
+
+struct colaNota{
+    Nodo * frente;
+
+    colaNota(){
+        frente = 0;
+    }
+    void encolar (int dato);
+    Nodo* desencolar (void);
+    Nodo* verFrente(void);
+    bool vacia(void);
+};
+
+struct colaDiablito{
+    Nodo * frente;
+
+    colaDiablito(){
+        frente = 0;
+    }
+    void encolar (int dato);
+    Nodo* desencolar (void);
+    Nodo* verFrente(void);
+    bool vacia(void);
+};
+
+struct colaSentimientos{
+    Nodo * frente;
+
+    colaSentimientos(){
+        frente = 0;
+    }
+    void encolar (int dato);
+    Nodo* desencolar (void);
+    Nodo* verFrente(void);
+    bool vacia(void);
+};
--- Bebes/main.cpp
+++ Bebes/main.cpp
@@ -4,6 +4,8 @@
 
 int main(int argc, char *argv[])
 {
+    printf("hola mundo");
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
